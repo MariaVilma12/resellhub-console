5c3e75f1180a71e9b1008fb2fe984cd986cc7a3f
MariaVilma12 <vilmashekar26@gmail.com>
2026-03-14 13:48:19 +0100

    Added User registration and login

---
+++ Services/users/RegisterService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.Sqlite;
+using ResellHubConsole.Models;
+using ResellHubConsole.Data; 
+namespace ResellHubConsole.Services.Users
+{
+    public class RegisterService
+    {
+        public void Register(string username, string password)
+        {
+            using var connection = Database.GetConnection();
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText =
+                @"INSERT INTO Users (Username, Password)
+              VALUES ($username, $password);";
+
+            command.Parameters.AddWithValue("$username", username);
+            command.Parameters.AddWithValue("$password", password);
+
+            command.ExecuteNonQuery();
+        }
+    }
+}
